37ca71cb853171dae1b03105f806cce3671f22a2
Luhan Lacerda <luhanlacerda@gmail.com>
2017-02-20 19:01:59 -0300

    Ajustes no menu da tela principal

--- GerenciamentoDeClinica/Telas/TelaPrincipal.Designer.cs
+++ GerenciamentoDeClinica/Telas/TelaPrincipal.Designer.cs
@@ -52,7 +52,7 @@
             this.consultasToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(379, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(393, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -140,7 +140,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(379, 271);
+            this.ClientSize = new System.Drawing.Size(393, 277);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.Name = "TelaPrincipal";
