cdf5b988873026e796ededd5cda1bb8da398349c
Luhan Lacerda <luhanlacerda@gmail.com>
2017-02-23 14:55:56 -0300

    Codificando a camada de negocio. Atualizar e remover

--- GerenciamentoDeClinica/Paciente/NegocioPaciente.cs
+++ GerenciamentoDeClinica/Paciente/NegocioPaciente.cs
@@ -8,7 +8,7 @@ namespace GerenciamentoDeClinica.Paciente
 {
     class NegocioPaciente : InterfacePaciente
     {
-        public void atualizar(Paciente p)
+        public void cadastrar(Paciente p)
         {
             if(p.getCpf().Trim().Length < 14 || p.getCpf().Trim().Length > 14)
             {
@@ -33,6 +33,11 @@ namespace GerenciamentoDeClinica.Paciente
             if (p.getRg().Trim().Equals("") == true || p.getRg() == null )
             {
                 throw new Exception("Favor informar o RG do paciente");
+            }
+
+            if (p.getConvenio().getCodigo() <= 0)
+            {
+                throw new Exception("Favor informar o convênio");
             }
 
             if (p.getContato().Trim().Length < 14)
@@ -84,11 +89,96 @@ namespace GerenciamentoDeClinica.Paciente
                 throw new Exception("Informar nome do país");
             }
 
+            //cadastrando
+            //colocar o codigo de jogar para a camada de dados
+
         }
 
-        public void cadastrar(Paciente p)
+        public void atualizar(Paciente p)
         {
-            throw new NotImplementedException();
+
+            if (p.getCpf().Trim().Length < 14 || p.getCpf().Trim().Length > 14)
+            {
+                throw new Exception("Número de CPF inválido!");
+            }
+
+            if (verificaExistencia(p) != false)
+            {
+                throw new Exception("Paciente já cadastrado no sistema!");
+            }
+
+            if (p.getNome().Trim().Equals("") == true || p.getNome() == null)
+            {
+                throw new Exception("Informar nome do paciente");
+            }
+
+            if (p.getNome().Trim().Length > 100)
+            {
+                throw new Exception("O nome do paciente não poderá ter mais de 100 caracteres");
+            }
+
+            if (p.getRg().Trim().Equals("") == true || p.getRg() == null)
+            {
+                throw new Exception("Favor informar o RG do paciente");
+            }
+
+            if (p.getConvenio().getCodigo() <= 0)
+            {
+                throw new Exception("Favor informar o convênio");
+            }
+
+            if (p.getContato().Trim().Length < 14)
+            {
+                throw new Exception("Número de telefone inválido");
+            }
+
+            if (p.getDtNascimento() == null)
+            {
+                throw new Exception("Informar data de nascimento");
+            }
+
+            if (p.getEndereco().getLogradouro().Trim().Equals("") || p.getEndereco().getLogradouro() == null)
+            {
+                throw new Exception("Informar nome da rua");
+            }
+
+            if (p.getEndereco().getNumero().Trim().Equals("") || p.getEndereco().getNumero() == null)
+            {
+                throw new Exception("Informar número da residência");
+            }
+
+            if (p.getEndereco().getComplemento().Trim().Equals("") || p.getEndereco().getComplemento() == null)
+            {
+                throw new Exception("Informar complemento, caso não tenha, colocar um -");
+            }
+
+            if (p.getEndereco().getCep().Trim().Equals("") || p.getEndereco().getCep() == null)
+            {
+                throw new Exception("Informar número do CEP");
+            }
+
+            if (p.getEndereco().getCidade().Trim().Equals("") || p.getEndereco().getCidade() == null)
+            {
+                throw new Exception("Informar nome da cidade");
+            }
+
+            if (p.getEndereco().getUf().Trim().Equals("") || p.getEndereco().getUf() == null)
+            {
+                throw new Exception("Informar nome da UF(Estado)");
+            }
+
+            if (p.getEndereco().getUf().Trim().Length > 2 || p.getEndereco().getUf().Trim().Length < 2)
+            {
+                throw new Exception("Informar UF com 2 caracteres");
+            }
+            if (p.getEndereco().getPais().Trim().Equals("") || p.getEndereco().getPais() == null)
+            {
+                throw new Exception("Informar nome do país");
+            }
+
+            //Atualizando
+            //colocar o codigo de jogar para a camada de dados
+
         }
 
         public List<Paciente> listar(Paciente filtro)
@@ -98,7 +188,18 @@ namespace GerenciamentoDeClinica.Paciente
 
         public void remover(Paciente p)
         {
-            throw new NotImplementedException();
+            if (p.getCpf().Trim().Length < 14 || p.getCpf().Trim().Length > 14)
+            {
+                throw new Exception("Número de CPF inválido!");
+            }
+
+            if (verificaExistencia(p) == false)
+            {
+                throw new Exception("Paciente não cadastrado no sistema!");
+            }
+
+            //Atualizando
+            //colocar o codigo de jogar para a camada de dados
         }
 
         public Paciente selecionarPaciente(Paciente p)
