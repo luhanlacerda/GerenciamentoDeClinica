97c66e5ebc3b0696cc7208a79a2af32000d5e3af
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2017-05-08 21:37:18 -0300

    Commite atualizado

--- Biblioteca/convenio/IConvenio.cs
+++ Biblioteca/convenio/IConvenio.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Biblioteca.convenio
 {
     public interface IConvenio
     {
         //Cadastrar um convenio
-        void Cadastrar(Convenio c);
+        void Cadastrar(Convenio convenio);
         //Atualizar um convenio
-        void Atualizar(Convenio c);
+        void Atualizar(Convenio convenio);
         //Remover um convenio
-        void Remover(Convenio c);
-        //Devolver os convenios que se encaixam no filtro de pesquisa
+        void Remover(Convenio convenio);
+        //Selecionar os convenios que se encaixam no filtro de pesquisa
         List<Convenio> Listar(Convenio filtro);
         //Verificar se um convenio já está cadastrado
-        Boolean VerificaExistencia (Convenio c);
+        Boolean VerificaExistencia(Convenio convenio);
     }
 }
