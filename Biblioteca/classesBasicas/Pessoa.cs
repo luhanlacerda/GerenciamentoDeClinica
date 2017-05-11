119faa5b9e3caae78a7a392e63bf4132d68e500e
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2017-05-11 19:03:21 -0300

    Identando código da classe básica Pessoa

--- Biblioteca/classesBasicas/Pessoa.cs
+++ Biblioteca/classesBasicas/Pessoa.cs
@@ -8,14 +8,14 @@ namespace Biblioteca.classesBasicas
 {
     public abstract class Pessoa : Endereco
     {
-        public String Nome { get; set; }
-        public String RG { get; set; }
-        public String CPF { get; set; }
-        public Endereco Endereco { get; set; }
-        //public string Endereco { get; set; }
-        public String Celular { get; set; }
+        public String Nome           { get; set; }
+        public String RG             { get; set; }
+        public String CPF            { get; set; }
+        public Endereco Endereco     { get; set; }
+        //public string Endereco     { get; set; }
+        public String Celular        { get; set; }
         public DateTime DtNascimento { get; set; }
-        public String Email { get; set; }
-        public String Estado_Civil { get; set; }
+        public String Email          { get; set; }
+        public String Estado_Civil   { get; set; }
     }
 }
