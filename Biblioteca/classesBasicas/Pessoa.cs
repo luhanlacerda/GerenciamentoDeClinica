484850b30e6454bf9535726baf756d15f1b05fb9
Luhan Lacerda <luhanlacerda@gmail.com>
2017-05-11 19:01:04 -0300

    Ajuste na classe básica pessoa para incluir um endereco como  um tipo e não como string. *corrigir código nas ramificações*

--- Biblioteca/classesBasicas/Pessoa.cs
+++ Biblioteca/classesBasicas/Pessoa.cs
@@ -11,7 +11,8 @@ namespace Biblioteca.classesBasicas
         public String Nome { get; set; }
         public String RG { get; set; }
         public String CPF { get; set; }
-        public string Endereco { get; set; }
+        public Endereco Endereco { get; set; }
+        //public string Endereco { get; set; }
         public String Celular { get; set; }
         public DateTime DtNascimento { get; set; }
         public String Email { get; set; }
