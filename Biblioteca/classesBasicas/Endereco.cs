3a2c7ec9f2c310eef8a444204c30ea3d8b6538cb
Luhan Lacerda <luhanlacerda@gmail.com>
2017-05-12 10:23:47 -0300

    Ajustes de atributos nas classes de Endereco e Pessoa.

--- Biblioteca/classesBasicas/Endereco.cs
+++ Biblioteca/classesBasicas/Endereco.cs
@@ -8,13 +8,13 @@ namespace Biblioteca.classesBasicas
 {
     public abstract class Endereco
     {
-        public String Logradouro { get; set; }
-        public String Numero { get; set; }
-        public String Complemento { get; set; }
-        public String Bairro { get; set; }
-        public String Cidade { get; set; }
-        public String UF { get; set; }
-        public String CEP { get; set; }
-        public String Pais { get; set; }
+        public String Logradouro    { get; set; }
+        public String Numero        { get; set; }
+        public String Complemento   { get; set; }
+        public String Bairro        { get; set; }
+        public String Cidade        { get; set; }
+        public String UF            { get; set; }
+        public String CEP           { get; set; }
+        public String Pais          { get; set; }
     }
 }
